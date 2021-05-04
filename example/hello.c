8402732921aea33d81280866dff17e561b542d76
xueshi hu <hubachelar@gmail.com>
2021-05-05 00:46:36 +0800

    add hello.c as example

---
+++ example/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h> // malloc
+#include <string.h> // strcmp ..
+#include <stdbool.h> // bool false true
+#include <stdlib.h> // sort
+#include <limits.h> // INT_MAX
+#include <math.h> // sqrt
+#include <unistd.h> // sleep
+#include <assert.h> // assert
+#include <fcntl.h> // open
+
+#include "../dune/dune.h"
+
+int main(int argc, char *argv[]){
+
+	DUNE_ENTER;
+  printf("hello\n");
+  
+  return 0;
+}
